f58f4c9b53157028fd6d337eb0bad09e78bb04e5
Risher Randall <randallr18@mail.wlu.edu>
2018-05-30 21:15:16 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,20 +3,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  num2 - num1
 end
 
 def division(num1, num2)
+  num2 / num1
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num2 % num1
 end
 
 def square_root(num)
+  math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  num1 + ((num2 * num3) / num4)
 end
